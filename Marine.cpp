0ed6cdd57fcaaddc6c29127a09cb312daf495855
David <david_elvir@unitec.edu>
2017-05-12 15:27:50 -0600

    l

--- Marine.cpp
+++ Marine.cpp
@@ -10,16 +10,16 @@ Marine::Marine(char color,int x,inty):Pieza(color,x,y){
 
 
 
-Pieza*** Marine::Mover(Pieza*** tablero,Pieza p,int x,int y){
-  if(p.getColor = 'N'){
+Pieza*** Marine::Mover(Pieza*** tablero,Pieza* p,int x,int y){
+  if(p->getColor = 'N'){
     if(tablero[x][y]==nullptr){
       tablero[x][y] = p;
-      int i = p.getX;
-      int j = p.getY;
+      int i = p->getX;
+      int j = p->getY;
       tablero[i][j] = NULL;
     }
   }
-  if(Pieza.getColor = 'B'){
+  if(p->getColor = 'B'){
   
   }
   
