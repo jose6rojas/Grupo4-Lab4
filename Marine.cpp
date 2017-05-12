568a52fab8db9a0f1705a576f2a126992fd2a67d
David <david_elvir@unitec.edu>
2017-05-12 15:43:41 -0600

    arreglos clase marine

--- Marine.cpp
+++ Marine.cpp
@@ -11,7 +11,7 @@ Marine::Marine(char color,int x,int y):Pieza(color,x,y){
 
 
 Pieza*** Marine::Mover(Pieza*** tablero,Pieza* p,int x,int y){
-  if(p->getColor() = 'N'){
+  if(p->getColor() == 'N'){
     if(tablero[x][y]==NULL){
       tablero[x][y] = p;
       int i = p->getX();
@@ -19,7 +19,7 @@ Pieza*** Marine::Mover(Pieza*** tablero,Pieza* p,int x,int y){
       tablero[i][j] = NULL;
     }
   }
-  if(p->getColor() = 'B'){
+  if(p->getColor() == 'B'){
     if(tablero[x][y]==NULL){
       tablero[x][y] = p;
       int i = p->getX();
